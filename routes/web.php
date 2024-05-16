f36730f56d3d770188622930feb72c5984e12ec6
AnaFebriYanti <anafebriyanti844@gmail.com>
2024-05-16 12:21:50 +0700

    Auth group controller buat login

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,13 @@ use App\Http\Controllers\Auth\LoginController;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+// Route::get('/', function () {
+//     return view('home');
+// });
 
+Route::middleware('auth')->group(function () {
 //Home
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 //Route::get('/home', [HomeController::class, 'index']);
@@ -47,6 +49,7 @@ Route::get('/orders/edit', [OrdersController::class, 'edit']);
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 
-Route::get('/home', function () {
-    return view('home');
-})->middleware('auth');
+});
+// Route::get('/home', function () {
+//     return view('home');
+// })->middleware('auth');
